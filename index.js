6c9277ba906d381746101c284ad65406b50397a1
Jeremy Rice <jdavidrice@gmail.com>
2021-01-08 14:46:19 -0500

    create template for app

--- index.js
+++ index.js
@@ -1,13 +1,72 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const util = require('util');
+
+const writeFileAsync = util.promisify(fs.writeFile);
 
 // TODO: Create an array of questions for user input
-const questions = [];
+
+const promptUser = () => {
+  return inquirer.prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?',
+    },
+    {
+      type: 'input',
+      name: 'location',
+      message: 'Where are you from?',
+    },
+    {
+      type: 'input',
+      name: 'hobby',
+      message: 'What is your favorite hobby?',
+    },
+    {
+      type: 'input',
+      name: 'food',
+      message: 'What is your favorite food?',
+    },
+    {
+      type: 'input',
+      name: 'github',
+      message: 'Enter your GitHub Username',
+    },
+    {
+      type: 'input',
+      name: 'linkedin',
+      message: 'Enter your LinkedIn URL.',
+    },
+  ]);
+};
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+
+const generateMD = (answers) =>
+  `${answers.name}
+${answers.location}
+${answers.github}
+${answers.linkedin}
+${answers.hobby}
+${answers.food}`;
 
 // TODO: Create a function to initialize app
-function init() {}
+const init = async () => {
+  console.log('hi');
+  try {
+    const answers = await promptUser();
+
+    const md = generateMD(answers);
+
+    await writeFileAsync('README.md', md);
+
+    console.log('Successfully wrote to README.md');
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 // Function call to initialize app
 init();
