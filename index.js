eec64cc55b38d50e0f5700d81008c496e325480d
Jeremy Rice <jdavidrice@gmail.com>
2021-01-14 22:20:52 -0500

    Change single quotes to double and remove all but last link to top of page

--- index.js
+++ index.js
@@ -1,76 +1,76 @@
 // Packages needed for this application
-const inquirer = require('inquirer');
-const fs = require('fs');
-const util = require('util');
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
 const writeFileAsync = util.promisify(fs.writeFile);
 
 // Array of questions for user input
 const promptUser = () => {
   return inquirer.prompt([
     {
-      type: 'input',
-      name: 'title',
-      message: 'What is the name of this app?',
+      type: "input",
+      name: "title",
+      message: "What is the name of this app?",
     },
     {
-      type: 'input',
-      name: 'description',
-      message: 'What is a description of this app?',
+      type: "input",
+      name: "description",
+      message: "What is a description of this app?",
     },
     {
-      type: 'input',
-      name: 'deploy',
-      message: 'What is the URL of the deployed app?',
+      type: "input",
+      name: "deploy",
+      message: "What is the URL of the deployed app?",
     },
     {
-      type: 'input',
-      name: 'installation',
-      message: 'What are the installation instructions?',
+      type: "input",
+      name: "installation",
+      message: "What are the installation instructions?",
     },
     {
-      type: 'input',
-      name: 'usage',
-      message: 'What are the usage instructions?',
+      type: "input",
+      name: "usage",
+      message: "What are the usage instructions?",
     },
     {
-      type: 'list',
-      name: 'license',
-      message: 'What license should be applied to this project?',
-      choices: ['MIT', 'GNU GPL v3.0', 'GNU AGPL v3.0', 'BSD 2-Clause', 'BSD 3-Clause', 'No license'],
+      type: "list",
+      name: "license",
+      message: "What license should be applied to this project?",
+      choices: ["MIT", "GNU GPL v3.0", "GNU AGPL v3.0", "BSD 2-Clause", "BSD 3-Clause", "No license"],
     },
     {
-      type: 'input',
-      name: 'tests',
-      message: 'What are the tests that can be performed on this app?',
+      type: "input",
+      name: "tests",
+      message: "What are the tests that can be performed on this app?",
     },
     {
-      type: 'input',
-      name: 'contributing',
-      message: 'Who has contributed to this project?',
+      type: "input",
+      name: "contributing",
+      message: "Who has contributed to this project?",
     },
     {
-      type: 'input',
-      name: 'username',
-      message: 'What is your GitHub username?',
+      type: "input",
+      name: "username",
+      message: "What is your GitHub username?",
     },
     {
-      type: 'input',
-      name: 'email',
-      message: 'What is your email address?',
+      type: "input",
+      name: "email",
+      message: "What is your email address?",
     },
   ]);
 };
 // Function to write README file
 const init = async () => {
-  console.log('Hello. Answer the following questions to generate a README file.');
+  console.log("Hello. Answer the following questions to generate a README file.");
   try {
     const answers = await promptUser();
 
     const md = generateMD(answers);
 
-    await writeFileAsync('README.md', md);
+    await writeFileAsync("README.md", md);
 
-    console.log('Successfully wrote to README.md');
+    console.log("Successfully wrote to README.md");
   } catch (err) {
     console.log(err);
   }
@@ -79,54 +79,42 @@ const init = async () => {
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
   switch (license) {
-    case 'MIT':
-      return '[![GitHub license](https://img.shields.io/badge/license-MIT-brightgreen)](https://img.shields.io/badge/license-MIT-brightgreen)';
-      break;
-    case 'GNU GPL v3.0':
-      return '[![GitHub license](https://img.shields.io/badge/license-GNU_GPL_v3.0-brightgreen)](https://img.shields.io/badge/license-GNU_GPL_v3.0-brightgreen)';
-      break;
-    case 'GNU AGPL v3.0':
-      return '[![GitHub license](https://img.shields.io/badge/license-GNU_AGPL_v3.0-brightgreen)](https://img.shields.io/badge/license-GNU_AGPL_v3.0-brightgreen)';
-      break;
-    case 'BSD 2-Clause':
-      return '[![GitHub license](https://img.shields.io/badge/license-BSD_2_Clause-brightgreen)](https://img.shields.io/badge/license-BSD_2_Clause-brightgreen)';
-      break;
-    case 'BSD 3-Clause':
-      return '[![GitHub license](https://img.shields.io/badge/license-BSD_3_Clause-brightgreen)](https://img.shields.io/badge/license-BSD_3_Clause-brightgreen)';
-      break;
-    case 'No license':
+    case "MIT":
+      return "[![GitHub license](https://img.shields.io/badge/license-MIT-brightgreen)](https://img.shields.io/badge/license-MIT-brightgreen)";
+    case "GNU GPL v3.0":
+      return "[![GitHub license](https://img.shields.io/badge/license-GNU_GPL_v3.0-brightgreen)](https://img.shields.io/badge/license-GNU_GPL_v3.0-brightgreen)";
+    case "GNU AGPL v3.0":
+      return "[![GitHub license](https://img.shields.io/badge/license-GNU_AGPL_v3.0-brightgreen)](https://img.shields.io/badge/license-GNU_AGPL_v3.0-brightgreen)";
+    case "BSD 2-Clause":
+      return "[![GitHub license](https://img.shields.io/badge/license-BSD_2_Clause-brightgreen)](https://img.shields.io/badge/license-BSD_2_Clause-brightgreen)";
+    case "BSD 3-Clause":
+      return "[![GitHub license](https://img.shields.io/badge/license-BSD_3_Clause-brightgreen)](https://img.shields.io/badge/license-BSD_3_Clause-brightgreen)";
+    case "No license":
       return "";
-      break;
   }
 }
 // Function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
   switch (license) {
-    case 'MIT':
-      return 'https://choosealicense.com/licenses/mit/';
-      break;
-    case 'GNU GPL v3.0':
-      return 'https://choosealicense.com/licenses/gpl-3.0';
-      break;
-    case 'GNU AGPL v3.0':
-      return 'https://choosealicense.com/licenses/agpl-3.0/';
-      break;
-    case 'BSD 2-Clause':
-      return 'https://choosealicense.com/licenses/bsd-2-clause';
-      break;
-    case 'BSD 3-Clause':
-      return 'https://choosealicense.com/licenses/bsd-3-clause';
-      break;
-    case 'No license':
+    case "MIT":
+      return "https://choosealicense.com/licenses/mit/";
+    case "GNU GPL v3.0":
+      return "https://choosealicense.com/licenses/gpl-3.0";
+    case "GNU AGPL v3.0":
+      return "https://choosealicense.com/licenses/agpl-3.0/";
+    case "BSD 2-Clause":
+      return "https://choosealicense.com/licenses/bsd-2-clause";
+    case "BSD 3-Clause":
+      return "https://choosealicense.com/licenses/bsd-3-clause";
+    case "No license":
       return "";
-      break;
   }
 }
 // Function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-  if (license === 'No license') {
+  if (license === "No license") {
     return "";
   } else {
     return `
@@ -134,14 +122,13 @@ function renderLicenseSection(license) {
     
   This software has been created under the [${license}](${renderLicenseLink(license)}) license.
 
-  * [Return to Top](#Description)
   `;
   }
 }
 // Function that renders an entry in the TOC for the License section, if it was created
 // If there is no license, the entry is not rendered
 function renderTocLicenseEntry(license) {
-  if (license === 'No license') {
+  if (license === "No license") {
     return "";
   } else {
     return `* [License](#License)`;
@@ -173,13 +160,9 @@ ${renderTocLicenseEntry(answers.license)}
 
 ${answers.installation}
 
-* [Return to Top](#Description)
-
 ## Usage 
 
 ${answers.usage}
-
-* [Return to Top](#Description)
 
 ## Questions
 
@@ -191,17 +174,11 @@ Follow this link to see some of my other projects.
 
 [GitHub Profile](https://github.com/${answers.username})
 
-* [Return to Top](#Description)
-
-##
-
 ${renderLicenseSection(answers.license)}
 
 ## Contributing
 
   ${answers.contributing}
-
-* [Return to Top](#Description)
 
 ## Tests
 
